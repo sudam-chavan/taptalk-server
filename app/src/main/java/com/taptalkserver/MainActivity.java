14e4e9fc42f5f6dbc11bb3b429ac7c7d9ca0d1f3
sudam <sudam.chavan@gmail.com>
2015-05-27 22:41:44 +0530

    network diskovery service added

--- app/src/main/java/com/taptalkserver/MainActivity.java
+++ app/src/main/java/com/taptalkserver/MainActivity.java
@@ -1,17 +1,68 @@
 package com.taptalkserver;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
+    public static final String TAG = "##MainActivity##";
+    private NsdManager mNsdManager;
+    private NsdManager.RegistrationListener mRegistrationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerService(1234);
+    }
+
+    public void registerService(int port) {
+        NsdServiceInfo serviceInfo  = new NsdServiceInfo();
+        serviceInfo.setServiceName("MicService");
+        serviceInfo.setServiceType("_http._tcp.");
+        serviceInfo.setPort(port);
+
+        mRegistrationListener = new NsdManager.RegistrationListener() {
+
+            @Override
+            public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
+                // Save the service name.  Android may have changed it in order to
+                // resolve a conflict, so update the name you initially requested
+                // with the name Android actually used.
+                String mServiceName = NsdServiceInfo.getServiceName();
+                Log.d(TAG, "Service registered successfully Name: " + mServiceName);
+            }
+
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Registration failed!  Put debugging code here to determine why.
+                Log.d(TAG, "Service registered failed error: " + errorCode);
+            }
+
+            @Override
+            public void onServiceUnregistered(NsdServiceInfo arg0) {
+                // Service has been unregistered.  This only happens when you call
+                // NsdManager.unregisterService() and pass in this listener.
+                Log.d(TAG, "Service successfully unregistered");
+            }
+
+            @Override
+            public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Unregistration failed.  Put debugging code here to determine why.
+                Log.d(TAG, "Service unregistration failed error: " + errorCode);
+            }
+        };
+
+        mNsdManager = (NsdManager)getSystemService(Context.NSD_SERVICE);
+        mNsdManager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
     }
 
     @Override
